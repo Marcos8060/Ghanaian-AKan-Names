688f3c42ca620181189dfa32c7e46bd70d7db7c3
Marcos Ochieng <marcosgav80@gmail.com>
2021-11-13 14:33:52 +0300

    added the condition for the female gender

--- app.js
+++ app.js
@@ -29,7 +29,47 @@ if(day == 'Sunday' && genderInput.value == 'male'){
     const search = document.querySelector('.search');
     // insert text
     searchContainer.insertBefore(h6,search);
+} else if(day == 'Sunday' && genderInput.value == 'female'){
+    // create element
+    const h6 = document.createElement('h6');
+    // add class 
+    h6.className = 'alert alert-success text-center';
+    // text node
+    h6.appendChild(document.createTextNode(`Thank you ${nameInput.value}, your Akan name is Kwasi.`));
+    // search container
+    const searchContainer = document.querySelector('.searchContainer');
+    // search box
+    const search = document.querySelector('.search');
+    // insert text
+    searchContainer.insertBefore(h6,search);
+} else if(day == 'Monday' && genderInput.value == 'male'){
+     // create element
+     const h6 = document.createElement('h6');
+     // add class 
+     h6.className = 'alert alert-success text-center';
+     // text node
+     h6.appendChild(document.createTextNode(`Thank you ${nameInput.value}, your Akan name is Kwasi.`));
+     // search container
+     const searchContainer = document.querySelector('.searchContainer');
+     // search box
+     const search = document.querySelector('.search');
+     // insert text
+     searchContainer.insertBefore(h6,search);
+} else if(day == 'Monday' && genderInput.value == 'female'){
+    // create element
+    const h6 = document.createElement('h6');
+    // add class 
+    h6.className = 'alert alert-success text-center';
+    // text node
+    h6.appendChild(document.createTextNode(`Thank you ${nameInput.value}, your Akan name is Kwasi.`));
+    // search container
+    const searchContainer = document.querySelector('.searchContainer');
+    // search box
+    const search = document.querySelector('.search');
+    // insert text
+    searchContainer.insertBefore(h6,search);
 }
+
 
 
 
